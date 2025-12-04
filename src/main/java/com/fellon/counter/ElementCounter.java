d89304cccd2d7835bbf4a7f4f0fbc77e0263517d
fellon-dnb <fellon@ancevt.com>
2025-12-04 15:41:43 +0300

    feat: add countValues method using getOrDefault for element counter

--- src/main/java/com/fellon/counter/ElementCounter.java
+++ src/main/java/com/fellon/counter/ElementCounter.java
@@ -1,10 +1,27 @@
 package com.fellon.counter;
 
 
-public class ElementCounter
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.HashMap;
+import java.util.Map;
+
+public class ElementCounter {
+    static String[] words = {"a", "b", "a", "c", "b", "a", "1", "B", "A", "B", "A"};
+
+    public static <T> Map<T, Integer> countValues(T[] array) {
+        Map<T, Integer> map = new HashMap<>();
+        for (T element : array) {
+            map.put(element, map.getOrDefault(element, 0) + 1);
+
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> result = getResult();
+        System.out.println(result);
+    }
+
+    private static Map<String, Integer> getResult() {
+        return countValues(words);
     }
 }
